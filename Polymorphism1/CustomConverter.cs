740438a8c21b516e72af14eab212a20094b8b76e
OsipenkoMD <OsipenkoMD@309D-3>
2024-09-09 09:46:45 +0700

    Добавьте файлы проекта.

---
+++ Polymorphism1/CustomConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism1
+{
+    public class CustomConverter
+    {
+        public void Converter(string input, out int result)
+        {
+            result = Convert.ToInt32(input);
+        }
+        public void Converter(string input, out double result)
+        {
+            result = Convert.ToDouble(input);
+        }
+        public void Converter(string input, out bool result)
+        {
+            result = Convert.ToBoolean(input);
+        }
+    }
+}
---
+++ Polymorphism1/Program.cs
@@ -0,0 +1,7 @@
+﻿using Polymorphism1;
+using System;
+
+CustomConverter converter = new CustomConverter();
+converter.Converter("123", out int intResult);
+converter.Converter("123,45", out double doubleResult);
+converter.Converter("true", out bool boolResult);
